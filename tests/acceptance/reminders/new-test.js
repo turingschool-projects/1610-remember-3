8c9971e02a09f5516e3acd5502739d1e8643be57
chelle tuerk <michelletuerk@gmail.com>
2017-02-09 16:47:42 -0700

    create new reminder route/form
    Generate new reminder route, add form with title, date and notes inputs.
    Add new reminder acceptance test.

---
+++ tests/acceptance/reminders/new-test.js
@@ -0,0 +1,16 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'remember/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | reminders/new');
+
+test('visiting /reminders/new', function(assert) {
+  visit('/reminders');
+
+  andThen(function() {
+    fillIn('.reminder.title', 'Hello World');
+    click('button');
+  });
+  andThen(function() {
+    assert.equal(currentURL(), '/reminders/new')
+  })
+});

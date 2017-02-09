59651b4a315a1adc872976f5beb1fc49a58f3125
Eric Sayler <easayler@gmail.com>
2017-02-09 11:24:03 -0700

    add nested route, change faker data
    Add nested route `reminders/:reminder_id` for displaying individual
    reminders. Add generated test for new route. Updated faker data to
    use cooler fake data than lorem. Add a template to display an individual
    reminder, and change reminder-item template in list to show only the
    reminder title (which is the link to view that individual reminder).
    Add a `findRecord` in the reminder route to display an individual
    reminder's data from the store.

--- mirage/factories/reminder.js
+++ mirage/factories/reminder.js
@@ -1,8 +1,8 @@
 import { Factory, faker } from 'ember-cli-mirage';
 
 export default Factory.extend({
-  title: () => faker.lorem.words().capitalize(),
+  title: () => faker.company.bs().capitalize(),
   date: () => faker.date.recent(3),
-  notes: () => faker.lorem.paragraph(),
+  notes: () => faker.hacker.phrase(),
   pinned: false
 });

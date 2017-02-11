e8644fc398c3f66d889914c04758befdf3dba443
Eric Sayler <easayler@gmail.com>
2017-02-11 12:34:47 -0700

    add moment and pikaday date picker

--- app/components/new-reminder.js
+++ app/components/new-reminder.js
@@ -7,14 +7,12 @@ export default Ember.Component.extend({
   classNames: ['new-reminder'],
 
   title: '',
-  date: '',
+  date: new Date(),
   notes: '',
 
   actions: {
     createReminder() {
       const reminder = this.getProperties('title', 'date', 'notes');
-      reminder.date = new Date(reminder.date);
-
       this.get('store').createRecord('reminder', reminder).save().then(() => {
         this.setProperties({ name: '', date: '', notes: '' });
       });

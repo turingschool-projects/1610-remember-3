0493e510374df46307dea9cea902fb9c50e58300
Eric Sayler <easayler@gmail.com>
2017-02-15 13:44:06 -0700

    add edit reminder acceptance test

--- tests/acceptance/reminders/edit-test.js
+++ tests/acceptance/reminders/edit-test.js
@@ -3,6 +3,25 @@ import moduleForAcceptance from 'remember/tests/helpers/module-for-acceptance';
 
 moduleForAcceptance('Acceptance | reminders/edit');
 
+test('editing a reminder title changes its title in list', function(assert) {
+  server.createList('reminder', 5);
+
+  visit('/reminders/edit/1');
+
+  andThen(function() {
+    fillIn('.reminder-title', 'Hello World');
+    click('.btn-update');
+
+  });
+
+  andThen(function() {
+    const titles = find('h3', '.reminders-list');
+    const titleFind = titles.filter(function() { return this.innerText === 'Hello World' })
+    assert.equal(titleFind.length, 1);
+  })
+
+});
+
 test('editing a reminder returns user to reminder item', function(assert) {
   server.createList('reminder', 5);
 

6c1740894d6db75a6fcd678bd418c8645f7ccd51
chelle tuerk <michelletuerk@gmail.com>
2017-02-14 17:57:59 -0700

    route back to reminder.id when save is clicked on updated reminder

--- app/components/edit-reminder.js
+++ app/components/edit-reminder.js
@@ -3,13 +3,20 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
   store: Ember.inject.service(),
+  router: Ember.inject.service('-routing'),
   tagName: 'section',
   classNames: ['edit-reminder'],
 
   title: '',
   date: '',
-  notes: ''
+  notes: '',
 
-
-
+  actions: {
+    editReminder() {
+      this.model.setProperties('title', 'date', 'notes');
+      this.model.save().then(() => {
+        this.get('router').transitionTo('reminders.reminder', [this.model.id.toString()]);
+      })
+    }
+  }
 });

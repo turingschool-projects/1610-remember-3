1b10b1e7f23c827d8cadf5451ec55976360ed493
Eric Sayler <easayler@gmail.com>
2017-02-15 19:23:55 -0700

    nest edit route inside reminders/reminder route

--- app/routes/reminders/edit.js
+++ app/routes/reminders/edit.js
@@ -1,8 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model(params) {
-    return this.get('store').findRecord('reminder', params.reminder_id);
-  },
+  // model(params) {
+  //   return this.get('store').findRecord('reminder', params.reminder_id);
+  // },
 
 });
